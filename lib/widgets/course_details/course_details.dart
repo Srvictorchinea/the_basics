ed6fae71fe03b11a96fe8f88da00c1c780180912
Srvictorchinea <sr.victorchinea@gmail.com>
2024-03-07 23:18:05 +0000

    [ADD] course details resonsive

--- lib/widgets/course_details/course_details.dart
+++ lib/widgets/course_details/course_details.dart
@@ -1,36 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class CourseDetails extends StatelessWidget {
   const CourseDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 600,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'FLUTTER WEB. \nTHE BASICS',
-            style: TextStyle(
-              fontWeight: FontWeight.w800,
-              height: 0.9,
-              fontSize: 80,
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      var textAligment =
+          sizingInformation.deviceScreenType == DeviceScreenType.desktop
+              ? TextAlign.left
+              : TextAlign.center;
+      double titleSize =
+          sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 50
+              : 80;
+      double descriptionSize =
+          sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 16
+              : 21;
+
+      return Container(
+        width: 600,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'FLUTTER WEB. \nTHE BASICS',
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                height: 0.9,
+                fontSize: titleSize,
+              ),
+              textAlign: textAligment,
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            'Flutter is an open-source UI software development toolkit created by Google. It is used to build natively compiled applications for mobile, web, and desktop from a single codebase. Flutter enables developers to create high-performance.',
-            style: TextStyle(
-              fontSize: 21,
-              height: 1.7,
+            SizedBox(
+              height: 30,
             ),
-          ),
-        ],
-      ),
-    );
+            Text(
+              'Flutter is an open-source UI software development toolkit created by Google. It is used to build natively compiled applications for mobile, web, and desktop from a single codebase. Flutter enables developers to create high-performance.',
+              style: TextStyle(
+                fontSize: descriptionSize,
+                height: 1.7,
+              ),
+              textAlign: textAligment,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
